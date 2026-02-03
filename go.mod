b5d4da38165bbce101d7936d3267ab905a2c811a
Gleb28 <iamgleb.g@gmail.com>
2026-02-03 23:33:39 +0300

    refactor: replaced godotenv with cleanenv

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module memetgbot
 go 1.25
 
 require (
-	github.com/joho/godotenv v1.5.1
+	github.com/ilyakaznacheev/cleanenv v1.5.0
 	github.com/looplab/fsm v1.0.3
 	github.com/lrstanley/go-ytdlp v1.2.7
 	gopkg.in/telebot.v4 v4.0.0-beta.7
@@ -12,13 +12,17 @@ require (
 )
 
 require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
 	github.com/ProtonMail/go-crypto v1.3.0 // indirect
 	github.com/cloudflare/circl v1.6.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
 	github.com/ulikunitz/xz v0.5.13 // indirect
 	golang.org/x/crypto v0.41.0 // indirect
 	golang.org/x/sys v0.35.0 // indirect
 	golang.org/x/text v0.28.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
--- internal/core/config/config.go
+++ internal/core/config/config.go
@@ -3,77 +3,37 @@ package config
 import (
 	"log"
 	"os"
-	"strconv"
 
-	g "github.com/joho/godotenv"
+	"github.com/ilyakaznacheev/cleanenv"
 )
 
 type AppConfig struct {
-	TgBotToken     string
-	LoggerBotToken string
-	AdminID        int64
-	ActivationKey  string
-	CookiesPath    string
-	YtdlpPath      string
-	FfmpegPath     string
-	IsDebug        bool
-	Database       DatabaseConfig
+	TgBotToken     string         `env:"TG_BOT_TOKEN" env-required:"true"`
+	LoggerBotToken string         `env:"LOGGER_BOT_TOKEN"`
+	AdminID        int64          `env:"ADMIN_ID" env-required:"true"`
+	ActivationKey  string         `env:"ACTIVATION_KEY" env-required:"true"`
+	CookiesPath    string         `env:"COOKIES_PATH" env-required:"true"`
+	YtdlpPath      string         `env:"YTDLP_PATH" env-required:"true"`
+	FfmpegPath     string         `env:"FFMPEG_PATH" env-required:"true"`
+	IsDebug        bool           `env:"IS_DEBUG" env-default:"false"`
+	Database       DatabaseConfig `env-required:"true"`
 }
 
 type DatabaseConfig struct {
-	FileName string
+	FileName string `env:"DB_FILE_NAME" env-required:"true"`
 }
 
 func MustConfig() *AppConfig {
+	config := &AppConfig{}
+
 	if os.Getenv("IS_DOCKERIZED") != "true" {
-		err := g.Load()
-		if err != nil {
-			log.Fatal("Error loading .env file (might be missing in non-dockerized env):", err.Error())
+		if err := cleanenv.ReadConfig(".env", config); err != nil {
+			log.Fatal("Error loading .env file (might be missing in non-dockerized env): ", err.Error())
 		}
 	}
 
-	config := &AppConfig{}
-
-	config.TgBotToken = os.Getenv("TG_BOT_TOKEN")
-	if config.TgBotToken == "" {
-		log.Fatal("environment variable TG_BOT_TOKEN is not set")
-	}
-
-	config.LoggerBotToken = os.Getenv("LOGGER_BOT_TOKEN")
-
-	adminId, err := strconv.Atoi(os.Getenv("ADMIN_ID"))
-	if err != nil {
-		log.Fatal("environment variable ADMIN_ID is not set")
-	}
-	config.AdminID = int64(adminId)
-
-	config.ActivationKey = os.Getenv("ACTIVATION_KEY")
-	if config.ActivationKey == "" {
-		log.Fatal("environment variable ACTIVATION_KEY is not set")
-	}
-
-	config.CookiesPath = os.Getenv("COOKIES_PATH")
-	if config.CookiesPath == "" {
-		log.Fatal("environment variable COOKIES_PATH is not set")
-	}
-
-	config.YtdlpPath = os.Getenv("YTDLP_PATH")
-	if config.YtdlpPath == "" {
-		log.Fatal("environment variable YTDLP_PATH is not set")
-	}
-
-	config.FfmpegPath = os.Getenv("FFMPEG_PATH")
-	if config.FfmpegPath == "" {
-		log.Fatal("environment variable FFMPEG_PATH is not set")
-	}
-
-	config.IsDebug = os.Getenv("IS_DEBUG") == "true"
-
-	config.Database = DatabaseConfig{
-		FileName: os.Getenv("DB_FILE_NAME"),
-	}
-	if config.Database.FileName == "" {
-		log.Fatal("environment variable DB_FILE_NAME is not set")
+	if err := cleanenv.ReadEnv(config); err != nil {
+		log.Fatal("Error reading environment variables: ", err.Error())
 	}
 
 	return config
